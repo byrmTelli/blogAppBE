3fd13674974cb54f560eddab19405f8e16d23437
byrmTelli <byrm.telli.contact@gmail.com>
2024-07-30 15:40:31 +0300

    unit testing implementation  just started.

--- blogAppBE.WEB/Controllers/UserController.cs
+++ blogAppBE.WEB/Controllers/UserController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace blogAppBE.WEB.Controllers
 {   
-    [Authorize]
+    // [Authorize]
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController:ControllerBase
